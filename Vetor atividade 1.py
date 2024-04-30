cbe69bd594146e804fe43895f7ff89984586f6cf
erickbarbieripuc <167023009+erickbarbieripuc@users.noreply.github.com>
2024-04-30 17:50:50 -0300

    Add files via upload

---
+++ Vetor atividade 1.py
@@ -0,0 +1,37 @@
+# #Atividade 1
+# vetor = [1,2,3,4,5,6,7,8,9,10]
+# indice = int(input("Digite um indice:"))
+# elemento = vetor[indice]
+# print(elemento)
+
+# #Atividade 2
+# vetor = []
+# while True:
+#     print("Vetor atual:", vetor)
+#     print("Adicionar um elemento = 1")
+#     print("Remover elemento = 2")
+#     print("Mostrar resultados finais = 3")
+    
+#     opcao = int(input("Digite a opção escolhida:"))
+    
+#     if opcao == 1:
+#         adicionar = input("Digite o elemento que deseja adicionar:")
+#         vetor.append(adicionar)
+#         print("Elemento adicionado com sucesso!")
+#     elif opcao == 2:
+#         remover = input("Digite o elemento que deseja remover:")
+#         if remover in vetor:
+#             vetor.remove(remover)
+#             print("Elemento removido com sucesso")
+#         else:
+#             print("Elemento não encontrado no vetor")
+#     elif opcao == 3:
+#         print("Vetor final:", vetor)
+#         break
+#     else:
+#         print("Esta opção não existe, tente novamente!")
+        
+#Atividade 3
+tamanho = int(input("Digite o tamanho do vetor:"))
+
+vetor = []
